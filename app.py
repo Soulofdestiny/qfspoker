c6010d748e432177c54037fe56e50cfa3a7518f4
nicksinger <nsinger@suse.de>
2018-07-09 12:31:32 +0200

    Add first template

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-from bottle import route, run, template
+from bottle import route, run, template, jinja2_view
 @route('/')
 def index():
     return '<b>Nothing to see here</b>!'
 
-run(host='localhost', port=8080)
+@route("/test")
+@jinja2_view("test.tpl", template_lookup=["assets"])
+def foobar():
+    return { "text" : "test-template" }
+
+run(host='0.0.0.0', port=8080)
